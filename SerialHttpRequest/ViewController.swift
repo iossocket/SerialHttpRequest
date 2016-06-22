247d3595e4714b311d21e8aa111c915a16eecbdd
iossocket <avx302@gmail.com>
2016-06-22 21:32:19 +0800

    init project

---
+++ SerialHttpRequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SerialHttpRequest
+//
+//  Created by Xueliang Zhu on 6/22/16.
+//  Copyright © 2016 kotlinchina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
